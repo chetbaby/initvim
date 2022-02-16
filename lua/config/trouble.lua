1c612e78107fc8dc3a559a83e107a512e92f3f97
chetbaby <chet.hay@gmail.com>
2022-02-16 18:13:59 -0500

    document diagnostics instead of workspace.

--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -1,14 +1,13 @@
 local noshow = { noremap = true, silent = true }
-local show = { noremap = true, silent = false }
 local keymap = vim.api.nvim_set_keymap
-
 keymap("", "<leader>die", ":TroubleToggle<CR>", noshow)
+
 require("trouble").setup({
 	position = "bottom", -- position of the list can be: bottom, top, left, right
 	height = 10, -- height of the trouble list when position is top or bottom
 	width = 50, -- width of the list when position is left or right
 	icons = true, -- use devicons for filenames
-	mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+	mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
 	fold_open = "", -- icon used for open folds
 	fold_closed = "", -- icon used for closed folds
 	group = true, -- group results by file
